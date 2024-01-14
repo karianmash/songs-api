a4863fcc93b8a93e7f52d621c8faf3871c1e47f2
Ian Macharia <ian@pesira.io>
2024-01-14 10:29:40 +0300

    Initial commit

---
+++ src/test/java/io/karianmash/songsapi/SongsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.karianmash.songsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
