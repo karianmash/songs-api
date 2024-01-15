022e61cdc947535ab882cd85dc9e09ebe5307fd1
Ian Macharia <ian@pesira.io>
2024-01-15 07:36:13 +0300

    Minimal REST endpoints

--- src/main/java/io/karianmash/songsapi/SongsController.java
+++ src/main/java/io/karianmash/songsapi/SongsController.java
@@ -1,4 +1,38 @@
 package io.karianmash.songsapi;
 
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.prompt.Prompt;
+import org.springframework.ai.prompt.messages.UserMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class SongsController {
+    private final ChatClient chatClient;
+
+    @Autowired
+    public SongsController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/ai/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", chatClient.generate(message));
+    }
+
+    @GetMapping("/ai/generateStream")
+    public ChatResponse generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return chatClient.generate(prompt);
+    }
 }
